0ef3e5556233699e229cb703a1660f1165d4111f
Maryory <maryory.vale.5@gmail.com>
2026-04-16 20:51:21 -0500

    Configuracion Proveedores y Redes (main.tf)

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_network" "private_network" {
+  name = "network-${terraform.workspace}"
+}
